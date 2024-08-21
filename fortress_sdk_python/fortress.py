25783bb043e35b857a9fd51d57b47169d4c66e36
John Finberg <johnbfinberg@gmail.com>
2024-08-21 11:25:28 -0700

    refactor: tenant name to tenant id (#2)
    * refactor: tenant name to tenant id
    
    * feat: better doc strings

--- fortress_sdk_python/fortress.py
+++ fortress_sdk_python/fortress.py
@@ -20,7 +20,12 @@ class Fortress:
         self.__tenant_to_database = {}
 
     def connect_database(self, database_id: str) -> Connection:
-        """Connect to a database on the Fortress platform"""
+        """
+        Connect to a database on the Fortress platform
+
+        :param database_id: ID of the database
+        :return: Connection object to the database
+        """
         if database_id in self.__connection_cache:
             return self.__connection_cache[database_id]
 
@@ -39,26 +44,44 @@ class Fortress:
         self.__connection_cache[database_id] = connection
         return connection
 
-    def create_database(self, alias: str) -> str:
-        """Create a new database on the Fortress platform"""
+    def create_database(self, alias: str = "") -> str:
+        """
+        Create a new database on the Fortress platform
+        Returns the ID of the created
+
+        :param alias: Alias for the database (optional)
+        """
         return self.__fortress.create_database(alias=alias)
 
     def delete_database(self, database_id: str) -> None:
-        """Delete a database on the Fortress platform"""
+        """
+        Delete a database on the Fortress platform
+
+        :param database_id: ID of the database to delete
+        """
         self.__fortress.delete_database(database_id=database_id)
 
     def list_databases(self) -> list[Database]:
-        """List all databases on the Fortress platform"""
+        """
+        List all databases on the Fortress platform
+
+        :return: List of databases
+        """
         return self.__fortress.list_databases()
 
-    def connect_tenant(self, tenant_name: str) -> Connection:
-        """Connect to a tenant's database on the Fortress platform"""
-        if tenant_name in self.__tenant_to_database:
+    def connect_tenant(self, tenant_id: str) -> Connection:
+        """
+        Connect to a tenant's database on the Fortress platform
+
+        :param tenant_id: ID of the tenant
+        :return: Connection object to the tenant's database
+        """
+        if tenant_id in self.__tenant_to_database:
             return self.connect_database(
-                self.__connection_cache[self.__tenant_to_database[tenant_name]]
+                self.__connection_cache[self.__tenant_to_database[tenant_id]]
             )
 
-        response = self.__fortress.get_uri(tenant_name, "tenant")
+        response = self.__fortress.get_uri(tenant_id, "tenant")
 
         connection = PostgresClient(
             response.url,
@@ -68,22 +91,36 @@ class Fortress:
             response.database,
         ).connect()
 
-        self.__tenant_to_database[tenant_name] = response.database_id
+        self.__tenant_to_database[tenant_id] = response.database_id
         self.__connection_cache[response.database_id] = connection
         return connection
 
     def create_tenant(
-        self, tenant_name: str, alias: str, database_id: str = ""
+        self, tenant_id: str, alias: str = "", database_id: str = ""
     ) -> None:
-        """Create a new tenant on the Fortress platform"""
+        """
+        Create a new tenant on the Fortress platform
+
+        :param tenant_id: ID of the tenant
+        :param alias: Alias for the tenant (optional)
+        :param database_id: ID of the database to assign the tenant to or if not provided a database will be created (optional)
+        """
         self.__fortress.create_tenant(
-            tenant_name=tenant_name, alias=alias, database_id=database_id
+            tenant_id=tenant_id, alias=alias, database_id=database_id
         )
 
-    def delete_tenant(self, tenant_name: str) -> None:
-        """Delete a tenant on the Fortress platform"""
-        self.__fortress.delete_tenant(tenant_name=tenant_name)
+    def delete_tenant(self, tenant_id: str) -> None:
+        """
+        Delete a tenant on the Fortress platform
+
+        :param tenant_id: ID of the tenant to delete
+        """
+        self.__fortress.delete_tenant(tenant_id=tenant_id)
 
     def list_tenants(self) -> list[Tenant]:
-        """List all tenants on the Fortress platform"""
+        """
+        List all tenants on the Fortress platform
+
+        :return: List of tenants
+        """
         return self.__fortress.list_tenants()
